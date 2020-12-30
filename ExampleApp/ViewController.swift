edf268ee83227a96c91c41adb8766f76607fa15b
muta <muttaroni8827@gmail.com>
2020-12-31 00:49:55 +0900

    feat: create example app as target

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by k_muta on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
