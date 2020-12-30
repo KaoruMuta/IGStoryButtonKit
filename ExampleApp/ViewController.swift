e9c2e8937835d46780b819bc096fe9750204caa1
muta <muta@m.cs.osakafu-u.ac.jp>
2020-12-31 01:33:38 +0900

    feat: import IGStoryUI framework for ExampleApp

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import IGStoryUI
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var storyView: IGStoryView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        storyView.backgroundColor = .blue
     }
 
 
