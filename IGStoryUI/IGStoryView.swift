a2dc592b60036dcd6dfa651c74e54bc6c24b551b
muta <muttaroni8827@gmail.com>
2020-12-31 17:11:25 +0900

    feat: add border for circle

--- IGStoryUI/IGStoryView.swift
+++ IGStoryUI/IGStoryView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 @IBDesignable open class IGStoryView: UIView {
     
+    @IBInspectable open var borderWidth: CGFloat = 5
+    
     override public init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -21,5 +23,7 @@ import UIKit
     
     private func configure() {
         layer.cornerRadius = layer.frame.width / 2.0
+        layer.borderWidth = borderWidth
+        layer.borderColor = .init(gray: 0.0, alpha: 0.0)
     }
 }
