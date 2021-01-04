5c09b6adec0d80bf52ed028a8cd30ab9e778b85c
muta <muttaroni8827@gmail.com>
2021-01-04 20:47:04 +0900

    feat: detect button event via delegate

--- ExampleApp/StoryCell.swift
+++ ExampleApp/StoryCell.swift
@@ -12,15 +12,44 @@ class StoryCell: UICollectionViewCell {
     
     @IBOutlet private weak var storyButton: IGStoryButton!
     @IBOutlet private weak var descriptionLabel: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        storyButton.delegate = self
+        storyButton.addTarget(self, action: #selector(didTapped), for: .touchUpInside)
     }
     
     func configure(displayType: IGStoryButton.DisplayType, image: UIImage?, text: String) {
         storyButton.type = displayType
         storyButton.image = image
         descriptionLabel.text = text
+    }
+    
+    @objc func didTapped() {
+        switch storyButton.type {
+        case .seen:
+            storyButton.startAnimating()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+                self?.storyButton.stopAnimating()
+            }
+        case .unseen:
+            storyButton.startAnimating()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+                self?.storyButton.type = .seen
+                self?.storyButton.stopAnimating()
+            }
+        case .status, .none:
+            break
+        }
+        
+    }
+}
+
+
+extension StoryCell: IGStoryButtonDelegate {
+    func didLongPressed() {
+        print("Detect long press event via delegate!")
     }
 }
