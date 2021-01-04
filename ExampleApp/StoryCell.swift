5b2c491ad8b215432e232fee0af06c5bb5868b44
muta <muttaroni8827@gmail.com>
2021-01-04 17:54:59 +0900

    feat: renew ExampleApp

--- ExampleApp/StoryCell.swift
+++ ExampleApp/StoryCell.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import IGStoryUI
 
 class StoryCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var storyButton: IGStoryButton!
+    @IBOutlet private weak var descriptionLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    func configure(displayType: IGStoryButton.DisplayType, image: UIImage?, text: String) {
+        storyButton.type = displayType
+        storyButton.image = image
+        descriptionLabel.text = text
+    }
 }
