e413364ce35c76d140627937bce21df3812c1238
Kaoru Muta <muttaroni8827@gmail.com>
2023-04-05 00:56:08 +0900

    feat: add swiftui project

---
+++ ExampleApp/Example-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example-SwiftUI
+//
+//  Created by k_muta on 2023/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApp/Example-SwiftUI/Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_SwiftUIApp.swift
+//  Example-SwiftUI
+//
+//  Created by k_muta on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
